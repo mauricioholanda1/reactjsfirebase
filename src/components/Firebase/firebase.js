87d61a5368bd9faa88668c599fb21bebdaec8f30
Mauricio Holanda <maumauholanda0@gmail.com>
2020-03-09 22:22:42 -0300

    app com firebase linkado e singup feito

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,35 @@
+import app from "firebase/app";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyBJs_VvfBBTn-h3XKyZ4876JIjZHGcpoH8",
+  authDomain: "cadastrofirebase-mauricio.firebaseapp.com",
+  databaseURL: "https://cadastrofirebase-mauricio.firebaseio.com",
+  projectId: "cadastrofirebase-mauricio",
+  storageBucket: "cadastrofirebase-mauricio.appspot.com",
+  messagingSenderId: "741647790440",
+  appId: "1:741647790440:web:4bc8328c57bb62146066dd",
+  measurementId: "G-EN46NLZLLL"
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+  }
+
+  // *** Auth API ***
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
