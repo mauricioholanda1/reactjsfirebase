c4e19c06dd1ec159299abd87e2c1412ac5be66b5
Mauricio Holanda <maumauholanda0@gmail.com>
2020-03-11 21:37:32 -0300

    Reactstrap

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,8 +1,19 @@
 import React from "react";
+import {Button} from "reactstrap";
+
 const Landing = () => (
   <div>
     <h1>Landing Page</h1>
     <p>The Landing Page is accessible by every user.</p>
+    <div>
+      <Button color="primary">primary</Button>{" "}
+      <Button color="secondary">secondary</Button>{" "}
+      <Button color="success">success</Button>{" "}
+      <Button color="info">info</Button>{" "}
+      <Button color="warning">warning</Button>{" "}
+      <Button color="danger">danger</Button> <Button color="link">link</Button>
+    </div>
+    
   </div>
 );
 export default Landing;
