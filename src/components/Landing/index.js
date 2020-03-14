f91793f6371b50b306440cee54abd786d47b4350
Mauricio Holanda <maumauholanda0@gmail.com>
2020-03-14 09:05:26 -0300

    Removing BootsStrap

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,18 +1,9 @@
 import React from "react";
-import {Button} from "reactstrap";
 
 const Landing = () => (
   <div>
     <h1>Landing Page</h1>
     <p>The Landing Page is accessible by every user.</p>
-    <div>
-      <Button color="primary">primary</Button>{" "}
-      <Button color="secondary">secondary</Button>{" "}
-      <Button color="success">success</Button>{" "}
-      <Button color="info">info</Button>{" "}
-      <Button color="warning">warning</Button>{" "}
-      <Button color="danger">danger</Button> <Button color="link">link</Button>
-    </div>
     
   </div>
 );
