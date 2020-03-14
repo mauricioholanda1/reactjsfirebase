a3ebc3eab67cce02f4c81cee5775a8df6b0f8594
Mauricio Holanda <maumauholanda0@gmail.com>
2020-03-14 09:06:26 -0300

    Styling with material ui

--- src/components/Auth/SignIn/index.js
+++ src/components/Auth/SignIn/index.js
@@ -5,14 +5,17 @@ import {SignUpLink} from "../SignUp";
 import {PasswordForgetLink} from "../PasswordForget";
 import {withFirebase} from "../../Firebase";
 import * as ROUTES from "../../../constants/routes";
-const SignInPage = () => (
-  <div>
-    <h1>SignIn</h1>
-    <SignInForm />
-    <PasswordForgetLink />
-    <SignUpLink />
-  </div>
-);
+import {styled} from "@material-ui/core/styles";
+import {
+  Typography,
+  CardContent,
+  CardActions,
+  Card,
+  Container,
+  TextField,
+  Button
+} from "@material-ui/core";
+
 const INITIAL_STATE = {
   email: "",
   password: "",
@@ -39,33 +42,69 @@ class SignInFormBase extends Component {
   onChange = event => {
     this.setState({[event.target.name]: event.target.value});
   };
+
   render() {
     const {email, password, error} = this.state;
     const isInvalid = password === "" || email === "";
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="password"
-          value={password}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign In
-        </button>
-        {error && <p>{error.message}</p>}
-      </form>
+      <Container style={{display: "flex", justifyContent: "center"}}>
+        <MyCard variant="outlined">
+          <CardContent>
+            <MyTitle>SignIn</MyTitle>
+
+            <form style={{display: "grid"}} onSubmit={this.onSubmit}>
+              <MyTextField
+                name="email"
+                label="Email"
+                type="text"
+                placeholder="Email Address"
+                variant="outlined"
+                value={email}
+                onChange={this.onChange}
+              />
+              <MyTextField
+                name="password"
+                label="Password"
+                type="password"
+                variant="outlined"
+                placeholder="Password"
+                value={password}
+                onChange={this.onChange}
+              />
+              <MyButton variant="contained" disabled={isInvalid} type="submit">
+                Sign In
+              </MyButton>
+              {error && <p>{error.message}</p>}
+            </form>
+          </CardContent>
+
+          <CardActions>
+            <PasswordForgetLink />
+            <SignUpLink />
+          </CardActions>
+        </MyCard>
+      </Container>
     );
   }
 }
+
+const MyCard = styled(Card)({
+  maxWidth: 400
+});
+
+const MyTitle = styled(Typography)({
+  fontSize: 30,
+  textAlign: "center",
+  padding: 10
+});
+
+const MyTextField = styled(TextField)({
+  height: 50,
+  padding: 10,
+  margin: 5
+});
+
+const MyButton = styled(Button)({});
+
 const SignInForm = compose(withRouter, withFirebase)(SignInFormBase);
-export default SignInPage;
-export {SignInForm};
+export default SignInForm;
